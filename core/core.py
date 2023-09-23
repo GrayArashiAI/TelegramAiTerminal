01198044be7770e4751bf6448f1b714763d7b4f4
GrayArashiAI <grayarashiai@gmail.com>
2023-09-23 21:04:29 +0900

    2023/9/23

--- core/core.py
+++ core/core.py
@@ -9,6 +9,13 @@ def load_config(config:dict = {}):
             config.update(yaml.safe_load(y))
     return config
 
+def run_bot(bot_name:str, **command_list:dict[str,callable]):
+    config = load_config()[bot_name]
+    app = ApplicationBuilder().token(config['TOKEN']).build()
+    for cmd, function in command_list.items():
+        app.add_handler(CommandHandler(cmd, function))
+    app.run_polling()
+
 class Bot:
     def __init__(self, bot_name:str):
         self.config = load_config()[bot_name]
@@ -22,9 +29,3 @@ class Bot:
     def run(self):
         self.app.run_polling()
 
-def run_bot(bot_name:str, **command_list:dict[str,callable]):
-    config = load_config()[bot_name]
-    app = ApplicationBuilder().token(config['TOKEN']).build()
-    for cmd, function in command_list.items():
-        app.add_handler(CommandHandler(cmd, function))
-    app.run_polling()
